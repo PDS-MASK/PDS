ebe350d2ba62eab4be313cbd64d53af9f4462478
Amine <Amine@PC-Asus>
2018-02-13 15:50:21 +0100

    pool de connexion

---
+++ src/DatabasePoolConnectionPDS.java
@@ -0,0 +1,45 @@
+/*
+ * 
+ * 13-02-2018 1.1
+ * PDS : Projet MASK
+ * 
+ * Interface servant à réaliser un pool de connexion test
+ * Le pool de connexion permet de réutiliser les connexions physiques à la base de données ORACLE 
+ * qui prennent beaucoup de temps et les réaffecte pour des nouvelles demandes de connexions côté clients. 
+ * Il est possible de l’implémenter avec JDBC.
+ * 
+ * @AmineHamdi
+ */
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Timer;
+
+import javax.sql.ConnectionPoolDataSource;
+
+import oracle.jdbc.pool.OracleConnectionPoolDataSource;
+
+public interface DatabasePoolConnectionPDS {
+	
+	String dbserver = "jdbc";
+	String dbuser = "test";
+	String dbpassword = "test";
+
+	
+	//Construit un pool de connexion avec comme paramètre le PoolDataSource qui va stocker les connexions,
+	//le nombre maximum de connexion et le délai d'attente avant d'ouvrir une nouvelle connexion (en seconde)
+	void DBConnexionPoolDataSource(ConnectionPoolDataSource dataSource,int maxConnection, int timeout);
+	
+	//Intercepter une connexion entrante et l'intégrer au Pool
+	Connection getConnection();
+	
+	void resetPoolDataSource(ConnectionPoolDataSource dataSource);
+	
+	int getMaxConnections();
+	
+	void closeUnusedPooledConnection();
+	
+	void closeConnction();
+
+}
