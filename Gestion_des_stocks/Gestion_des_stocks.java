9f5d1ccf7dbb75deb71d7e07e3dbb2cbbfbecf7c
ahamdiupec <33027517+ahamdiupec@users.noreply.github.com>
2018-04-22 19:58:26 +0200

    Application
    Ces fichiers sont les premières vues de l'application.
    
    Deux packages : 
    
    - Un package " general " qui regroupe les vues visibles par tout le monde
    - Un package "Gestion_des_stocks" qui centralise uniquement les vues et fonctions liées à la gestion des stocks.
    
    Login.java : accès à une page de login simple avec deux champs : login et password. Si la connexion est réalisée, l'utilisateur est renvoyée vers le menu d'administration.
    
    Administration.java : Le menu principal d'administration qui regroupe les différentes fonctionnalités. Actuellement, lorsque l'on clique sur le bouton "Gestion des stocks" cela renvoie vers le menu de gestion des stocks.
    
    Gestion_des_stocks.java : vue simple de la gestion des stocks sans les fonctionnalités pour le moment.

---
+++ Gestion_des_stocks/Gestion_des_stocks.java
@@ -0,0 +1,54 @@
+package Gestion_des_stocks;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class Gestion_des_stocks extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Gestion_des_stocks frame = new Gestion_des_stocks();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Gestion_des_stocks() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblGestionDesStocks = new JLabel("Gestion des stocks");
+		lblGestionDesStocks.setFont(new Font("Berlin Sans FB", Font.BOLD, 25));
+		lblGestionDesStocks.setBounds(102, 10, 228, 41);
+		contentPane.add(lblGestionDesStocks);
+		
+		JButton btnGestionDesBons = new JButton("Gestion des bons de livraison");
+		btnGestionDesBons.setBounds(66, 230, 315, 21);
+		contentPane.add(btnGestionDesBons);
+	}
+
+}
