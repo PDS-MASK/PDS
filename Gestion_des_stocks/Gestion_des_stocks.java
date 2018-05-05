b0c3ce0c4eb767850a972891f59ccadae4cfce84
ahamdiupec <33027517+ahamdiupec@users.noreply.github.com>
2018-05-05 21:48:36 +0200

    Add files via upload
    ajout du jtable

--- Gestion_des_stocks/Gestion_des_stocks.java
+++ Gestion_des_stocks/Gestion_des_stocks.java
@@ -1,13 +1,14 @@
 package Gestion_des_stocks;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 
 import javax.swing.JButton;
@@ -15,11 +16,12 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
 import general.Administration;
-import javax.swing.JTable;
-import javax.swing.JScrollPane;
 
 public class Gestion_des_stocks extends JFrame {
 
@@ -41,29 +43,52 @@ public class Gestion_des_stocks extends JFrame {
 			}
 		});
 	}
-	
+
 	public void theQuery(String query){
 		Connection connection = null;
-		
 		try {
-		Class.forName("oracle.jdbc.OracleDriver");
-		connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","ahamdi","password");	
-		Statement stmt = null;
-		stmt = connection.createStatement();
-	    stmt.executeUpdate(query);
-	    JOptionPane.showMessageDialog(null,"Query Executed");
-	    
-	      }catch(Exception ex){
-	          ex.printStackTrace();
-	      }
-	  }
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","ahamdi","resident4");	
+			Statement stmt = null;
+			stmt = connection.createStatement();
+			ResultSet result = stmt.executeQuery(query);
+			ResultSetMetaData resultMeta = result.getMetaData();
+
+			while(result.next())
+			{          
+				for(int i = 1; i <=  resultMeta.getColumnCount(); i++)
+					System.out.print(result.getObject(i).toString());  
+			}
+			result.close();
+			stmt.close();
+			JOptionPane.showMessageDialog(null,"Query Executed");
+
+			/*
+			 *  String  sql = "SELECT adresse FROM customer WHERE login = " + login;
+    //System.out.println(sql);
+
+    Statement state = _conn.createStatement();
+    ResultSet result = state.executeQuery(sql);
+    ResultSetMetaData resultMeta = result.getMetaData();
+
+    while(result.next())
+    {          
+        for(int i = 1; i <=  resultMeta.getColumnCount(); i++)
+            System.out.print(result.getObject(i).toString());  
+    }
+    result.close();
+        state.close();
+			 */
+		}catch(Exception ex){
+			JOptionPane.showMessageDialog(null,ex.getMessage());
+		}
+	}
 
 	/**
 	 * Create the frame.
 	 */
 	public Gestion_des_stocks() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 812, 495);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -71,16 +96,16 @@ public class Gestion_des_stocks extends JFrame {
 
 		JLabel lblGestionDesStocks = new JLabel("Gestion des stocks");
 		lblGestionDesStocks.setFont(new Font("Berlin Sans FB", Font.BOLD, 25));
-		lblGestionDesStocks.setBounds(102, 10, 228, 41);
+		lblGestionDesStocks.setBounds(283, 10, 228, 41);
 		contentPane.add(lblGestionDesStocks);
 
-		JButton btnGestionDesBons = new JButton("Gestion des bons de livraison");
+		JButton btnGestionDesBons = new JButton("Test affichage des produits");
 		btnGestionDesBons.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				theQuery("SELECT * FROM ARTICLE");		
+				theQuery("SELECT * FROM ARTICLE");
 			}
 		});
-		btnGestionDesBons.setBounds(66, 230, 315, 21);
+		btnGestionDesBons.setBounds(239, 426, 315, 21);
 		contentPane.add(btnGestionDesBons);
 
 		JButton btnRetour = new JButton("Retour");
@@ -92,23 +117,43 @@ public class Gestion_des_stocks extends JFrame {
 		});
 		btnRetour.setBounds(0, 10, 85, 21);
 		contentPane.add(btnRetour);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 173, 416, -113);
-		contentPane.add(scrollPane);
-		
-		table = new JTable();
-		scrollPane.setViewportView(table);
-		
-		JButton btnAjouterUnArticle = new JButton("Ajouter un article");
-		btnAjouterUnArticle.setBounds(0, 198, 120, 21);
-		contentPane.add(btnAjouterUnArticle);
-		
-		JButton btnSupprimerUnArticle = new JButton("Supprimer un article");
-		btnSupprimerUnArticle.setBounds(146, 198, 140, 21);
-		contentPane.add(btnSupprimerUnArticle);
-		
 
-		
+		JButton btnAjouterUnArticle = new JButton("Ajouter un article");
+		btnAjouterUnArticle.setBounds(10, 390, 201, 21);
+		contentPane.add(btnAjouterUnArticle);
+
+		JButton btnSupprimerUnArticle = new JButton("Supprimer un article");
+		btnSupprimerUnArticle.setBounds(585, 390, 201, 21);
+		contentPane.add(btnSupprimerUnArticle);
+
+		table = new JTable();
+		table.setBounds(24, 157, 385, -106);
+		contentPane.add(table);
+
+		// create a table model and set a Column Identifiers to this model 
+		Object[] columns = {"Nom","Catégorie","Genre","Description"};
+		DefaultTableModel model = new DefaultTableModel();
+		model.setColumnIdentifiers(columns);
+
+		// set the model to the table
+		table.setModel(model);
+
+		// Change A JTable Background Color, Font Size, Font Color, Row Height
+		table.setBackground(Color.LIGHT_GRAY);
+		table.setForeground(Color.black);
+		Font font = new Font("",1,22);
+		table.setFont(font);
+		table.setRowHeight(30);
+
+		// create JScrollPane
+		JScrollPane pane = new JScrollPane(table);
+		pane.setBounds(10, 58, 776, 322);
+
+		getContentPane().setLayout(null);
+
+		getContentPane().add(pane);
+
+
+
 	}
 }
