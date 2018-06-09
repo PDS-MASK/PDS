7cc6fec8fcab4bfb35e9e675e8c3874984a5295e
ahamdiupec <33027517+ahamdiupec@users.noreply.github.com>
2018-06-09 19:44:45 +0200

    Add files via upload

--- Gestion_des_stocks/Page_principale.java
+++ Gestion_des_stocks/Page_principale.java
@@ -5,41 +5,32 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableModel;
 
-import tables.Article;
 import general.Administration;
 import net.proteanit.sql.DbUtils;
-
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
 public class Page_principale extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
 	private JTextField txtBoutique;
-	private JTextField txtArticle;
-	private JTextField txtTaille;
-	private JTextField txtCouleur;
-	private JTextField textStock;
 	Connection con = connect();
 	ResultSet rs;
 	PreparedStatement stat;
@@ -62,19 +53,20 @@ public class Page_principale extends JFrame {
 	}
 
 	public Connection connect() {
-        try {
-        	con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "pds", "");
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Erreur de connection a la base de donnees :" + e);
-        }
-        /*retourne un object con de type Connection*/
-        return con;
-    }
+		try {
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "ahamdi", "pds123");
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Erreur de connection a la base de donnees :" + e);
+		}
+		/*retourne un object con de type Connection*/
+		return con;
+	}
 
 	/**
 	 * Create the frame.
+	 * @throws SQLException 
 	 */
-	public Page_principale() {
+	public Page_principale() throws SQLException {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1120, 542);
 		contentPane = new JPanel();
@@ -82,19 +74,13 @@ public class Page_principale extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
+
+
 		JLabel lblGestionDesStocks = new JLabel("Gestion des stocks");
 		lblGestionDesStocks.setFont(new Font("Berlin Sans FB", Font.BOLD, 25));
 		lblGestionDesStocks.setBounds(357, 5, 228, 41);
 		contentPane.add(lblGestionDesStocks);
 
-		JButton btnGestionDesBons = new JButton("Affichage des produits");
-		btnGestionDesBons.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				consulter_stock();
-			}
-		});
-		btnGestionDesBons.setBounds(239, 426, 315, 21);
-		contentPane.add(btnGestionDesBons);
 
 		JButton btnRetour = new JButton("Retour");
 		btnRetour.addActionListener(new ActionListener() {
@@ -106,11 +92,11 @@ public class Page_principale extends JFrame {
 		btnRetour.setBounds(0, 10, 85, 21);
 		contentPane.add(btnRetour);
 
-		JButton btnAjouterUnArticle = new JButton("Ajouter un article");
+		JButton btnAjouterUnArticle = new JButton("Approvisionner le stock");
 		btnAjouterUnArticle.setBounds(10, 390, 201, 21);
 		contentPane.add(btnAjouterUnArticle);
 
-		JButton btnSupprimerUnArticle = new JButton("Supprimer un article");
+		JButton btnSupprimerUnArticle = new JButton("Mise � jour des stocks");
 		btnSupprimerUnArticle.setBounds(10, 436, 201, 21);
 		contentPane.add(btnSupprimerUnArticle);
 
@@ -118,13 +104,6 @@ public class Page_principale extends JFrame {
 		table.setBounds(24, 157, 385, -106);
 		contentPane.add(table);
 
-		// // create a table model and set a Column Identifiers to this model
-		// Object[] columns = {"Nom","Cat�gorie","Genre","Description"};
-		// DefaultTableModel model = new DefaultTableModel();
-		// model.setColumnIdentifiers(columns);
-		//
-		// // set the model to the table
-		// table.setModel(model);
 
 		// Change A JTable Background Color, Font Size, Font Color, Row Height
 		table.setBackground(Color.LIGHT_GRAY);
@@ -141,55 +120,66 @@ public class Page_principale extends JFrame {
 
 		getContentPane().add(pane);
 
-		JTextArea textBoutique = new JTextArea();
-		textBoutique.setBounds(10, 101, 127, 22);
-		contentPane.add(textBoutique);
-
-		JTextArea textArticle = new JTextArea();
-		textArticle.setBounds(10, 162, 127, 22);
-		contentPane.add(textArticle);
-
-		JTextArea textTaille = new JTextArea();
-		textTaille.setBounds(10, 223, 127, 22);
-		contentPane.add(textTaille);
-
-		JTextArea textDescription = new JTextArea();
-		textDescription.setBounds(10, 297, 127, 22);
-		contentPane.add(textDescription);
-
 		txtBoutique = new JTextField();
-		txtBoutique.setText("Nom boutique");
-		txtBoutique.setBounds(10, 67, 127, 19);
-		contentPane.add(txtBoutique);
+		txtBoutique.setText("Boutique");
 		txtBoutique.setColumns(10);
+		txtBoutique.setBounds(10, 64, 122, 19);
+		contentPane.add(txtBoutique);
 
-		txtArticle = new JTextField();
-		txtArticle.setText("Nom article");
-		txtArticle.setColumns(10);
-		txtArticle.setBounds(10, 133, 127, 19);
-		contentPane.add(txtArticle);
+		JButton btnNewButton = new JButton("Espace fournisseurs");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Gestion_des_stocks.Espace_fournisseur ef = new Gestion_des_stocks.Espace_fournisseur();
+				ef.setVisible(true);
+			}
+		});
+		btnNewButton.setBounds(729, 390, 182, 29);
+		contentPane.add(btnNewButton);
 
-		txtTaille = new JTextField();
-		txtTaille.setText("Taille");
-		txtTaille.setColumns(10);
-		txtTaille.setBounds(10, 194, 127, 19);
-		contentPane.add(txtTaille);
+		JButton btnSuiviDesActivits = new JButton("Suivi des activit\u00E9s");
+		btnSuiviDesActivits.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Gestion_des_stocks.Suivi s = new Gestion_des_stocks.Suivi();
+				s.setVisible(true);
+			}
+		});
+		btnSuiviDesActivits.setBounds(724, 432, 187, 29);
+		contentPane.add(btnSuiviDesActivits);
 
-		txtCouleur = new JTextField();
-		txtCouleur.setText("Couleur");
-		txtCouleur.setColumns(10);
-		txtCouleur.setBounds(10, 268, 127, 19);
-		contentPane.add(txtCouleur);
+		String nomBoutique;
+		JComboBox BoutiqueComboBox = new JComboBox();
+		BoutiqueComboBox.setBounds(10, 99, 122, 26);
+		contentPane.add(BoutiqueComboBox);
+		Statement sta = con.createStatement();
+		String Sql = "select nom_boutique from boutique";
+		ResultSet rs = sta.executeQuery(Sql);
+		while (rs.next()) {
+			nomBoutique = rs.getString("nom_boutique");
+			BoutiqueComboBox.addItem(nomBoutique);
+		}
 
-		textStock = new JTextField();
-		textStock.setText("Stock");
-		textStock.setColumns(10);
-		textStock.setBounds(10, 329, 127, 19);
-		contentPane.add(textStock);
-
-		JTextArea txtStock = new JTextArea();
-		txtStock.setBounds(10, 358, 127, 22);
-		contentPane.add(txtStock);
+		JButton btnGestionDesBons = new JButton("Affichage des produits");
+		btnGestionDesBons.setBounds(239, 426, 315, 21);
+		contentPane.add(btnGestionDesBons);
+		btnGestionDesBons.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String boutiqueSelectionnee = BoutiqueComboBox.getSelectedItem().toString();
+				System.out.println(boutiqueSelectionnee);
+				Statement sta;
+				try {
+					Statement sta2 = con.createStatement();
+					String Sql2 = "select * from boutique where boutique.nom_boutique ='" + boutiqueSelectionnee + "'";
+					ResultSet rs2 = sta2.executeQuery(Sql2);
+					rs2.next();
+					String i = rs2.getString("id_boutique");
+					System.out.println("!!!!!!!!!rs : " + i + "!!!!!!!");
+					showTableData(i);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
 
 
 		// create an array of objects to set the row data
@@ -201,13 +191,7 @@ public class Page_principale extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
-				row[0] = textBoutique.getText();
-				row[1] = textArticle.getText();
-				row[2] = textTaille.getText();
-				row[3] = textDescription.getText();
-				
-				Article a = new Article();
-				rentrer_article(a);
+
 
 			}
 		});
@@ -231,27 +215,7 @@ public class Page_principale extends JFrame {
 			}
 		});
 
-		// // button update row
-		// btnModifierUnArticle.addActionListener(new ActionListener(){
-		//
-		// @Override
-		// public void actionPerformed(ActionEvent e) {
-		//
-		// // i = the index of the selected row
-		// int i = table.getSelectedRow();
-		//
-		// if(i >= 0)
-		// {
-		// table.getModel().setValueAt(textNom.getText(), i, 0);
-		// table.getModel().setValueAt(textCategorie.getText(), i, 1);
-		// table.getModel().setValueAt(textGenre.getText(), i, 2);
-		// table.getModel().setValueAt(textDescription.getText(), i, 3);
-		// }
-		// else{
-		// System.out.println("Update Error");
-		// }
-		// }
-		// });
+
 
 		showTableData();
 
@@ -259,10 +223,10 @@ public class Page_principale extends JFrame {
 
 	public void showTableData() {
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "ahamdi", "");
+			//Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "ahamdi", "resident4");
 			//String sql = "SELECT NOM_BOUTIQUE, NOM_ARTICLE, TAILLE, COULEUR, STOCK FROM ARTICLE INNER JOIN SOUS_ARTICLE ON ARTICLE.ID_ARTICLE = SOUS_ARTICLE.ID_ARTICLE INNER JOIN BOUTIQUE ON BOUTIQUE.ID_BOUTIQUE = SOUS_ARTICLE.ID_BOUTIQUE";
-			String sql = "SELECT * FROM ARTICLE";
-			Object pst = connection.prepareStatement(sql);
+			String sql = "SELECT NOM_ARTICLE, CATEGORIE, QUANTITE FROM ARTICLE";
+			Object pst = con.prepareStatement(sql);
 			Object rs = ((PreparedStatement) pst).executeQuery();
 			table.setModel(DbUtils.resultSetToTableModel((ResultSet) rs));
 			System.out.print(" " + rs);
@@ -273,69 +237,62 @@ public class Page_principale extends JFrame {
 		}
 
 	}
-	
-	 public Object[][] consulter_stock() {
-	        Object[][] donnes = new Object[100][4];
 
-	        try {
-	            /*initialisation de l'object con de type Connection et etablissement de la connection*/
-	            con = connect();
-	            String r = "select * from Article_Stock";
-	            /*initialisation de lobjet stat de type prepareStatement*/
-	            s = con.createStatement();
-	            rs = s.executeQuery(r);
-	            int i = 0;
-	            while (rs.next()) {
-	                donnes[i][0] = rs.getString("ID_Article");
-	                donnes[i][1] = rs.getString("Nom_Article");
-	                donnes[i][2] = rs.getString("Categorie");
-	                donnes[i][3] = rs.getString("Quantite");
-	                i++;
-	            }
-	        } catch (Exception e) {
-	        }
-	        return donnes;
-	    }
-	 
-	 public void rentrer_article(tables.Article a) {
-	        /*recuperation des membres du bean Article deja rempli*/
-	        int id_article = a.getId_Article();
-	        String nom_article = a.getNom_Article();
-	        String categorie = a.getCategorie();
-	        int quantite = a.getQuantite();
-	        try {
-	            /*creation de la requete pour linsertion dans la table Article_Stock*/
-	            stat = con.prepareStatement("insert into Article_Stock values (?,?,?,?)");
-	            /*initialisation des valeur de lobjet prepareStatement avec les
+	public void showTableData(String s) {
+		try {
+			//Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "ahamdi", "resident4");
+			//String sql = "SELECT NOM_BOUTIQUE, NOM_ARTICLE, TAILLE, COULEUR, STOCK FROM ARTICLE INNER JOIN SOUS_ARTICLE ON ARTICLE.ID_ARTICLE = SOUS_ARTICLE.ID_ARTICLE INNER JOIN BOUTIQUE ON BOUTIQUE.ID_BOUTIQUE = SOUS_ARTICLE.ID_BOUTIQUE";
+			String sql = "SELECT NOM_ARTICLE, CATEGORIE, QUANTITE FROM ARTICLE WHERE ARTICLE.ID_BOUTIQUE = " + s;
+			Object pst = con.prepareStatement(sql);
+			Object rs = ((PreparedStatement) pst).executeQuery();
+			table.setModel(DbUtils.resultSetToTableModel((ResultSet) rs));
+			System.out.print(" " + rs);
+
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, ex);
+
+		}
+
+	}
+
+
+
+	public void rentrer_article(tables.Article a) {
+		/*recuperation des membres du bean Article deja rempli*/
+		int id_article = a.getId_Article();
+		String nom_article = a.getNom_Article();
+		String categorie = a.getCategorie();
+		int quantite = a.getQuantite();
+		try {
+			/*creation de la requete pour linsertion dans la table Article_Stock*/
+			stat = con.prepareStatement("insert into Article_Stock values (?,?,?,?)");
+			/*initialisation des valeur de lobjet prepareStatement avec les
 	            valeures recuperéés a partir du bean Article accepté comme parametre dentree*/
-	            stat.setInt(1, id_article);
-	            stat.setString(2, nom_article);
-	            stat.setString(3, categorie);
-	            stat.setInt(4, quantite);
-	            /*execution de la requete*/
-	            int i = stat.executeUpdate();
-	            if (i != 0) {
-	                JOptionPane.showMessageDialog(null, "Article N°:" + id_article + "\nEn Stock ");
+			stat.setInt(1, id_article);
+			stat.setString(2, nom_article);
+			stat.setString(3, categorie);
+			stat.setInt(4, quantite);
+			/*execution de la requete*/
+			int i = stat.executeUpdate();
+			if (i != 0) {
+				JOptionPane.showMessageDialog(null, "Article N°:" + id_article + "\nEn Stock ");
 
-	            } else {
-	                JOptionPane.showMessageDialog(null, "Erreur de traitement");
-	            }
-	            /*Creation de la requete pour supprimer la ligne recuperée de la
+			} else {
+				JOptionPane.showMessageDialog(null, "Erreur de traitement");
+			}
+			/*Creation de la requete pour supprimer la ligne recuperée de la
 	            table Commande et inserée das la table Article_Stock*/
-	            stat = con.prepareStatement("delete from Commande where ID_Com=?");
-	            stat.setInt(1, id_article);
-	            int ex = stat.executeUpdate();
-	            if (ex != 0) {
-	                JOptionPane.showMessageDialog(null, "Supression Article N°:" + id_article + "\nDe la table Commande");
-	            } else {
-	                JOptionPane.showMessageDialog(null, "Erreur de traitement");
-	            }
+			stat = con.prepareStatement("delete from Commande where ID_Com=?");
+			stat.setInt(1, id_article);
+			int ex = stat.executeUpdate();
+			if (ex != 0) {
+				JOptionPane.showMessageDialog(null, "Supression Article N°:" + id_article + "\nDe la table Commande");
+			} else {
+				JOptionPane.showMessageDialog(null, "Erreur de traitement");
+			}
 
-	        } catch (Exception e) {
-	            JOptionPane.showMessageDialog(null, e);
-	        }
-	    }
-	 
-	 
-
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+		}
+	}
 }
